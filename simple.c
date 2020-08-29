623284f0d907e22d9afa0708b9ad434cd0db61cb
a-weed <ajweeden2016@gmail.com>
2020-08-29 18:01:35 -0500

    Testing ssh commits to track on github

---
+++ simple.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int x = 69420;
+int main() {
+	printf("Hello World!");
+	int i = 69420;
+	return 0;
+	 x = 420;
+	 x = 69;
+	 i = 420;
+}
---
+++ simpless.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+
+int x = 69420;
+
+int main() {
+	
+	int i = 0;
+	printf("Hello World!");
+
+}
