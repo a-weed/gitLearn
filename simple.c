40c934364ce1c63e4446e698d9ad3ce6309534d5
a-weed <ajweeden2016@gmail.com>
2020-08-29 19:42:01 -0500

    Basic loop test: write and edit in vim, compile through gcc, debug in gdb, and commit

--- simple.c
+++ simple.c
@@ -3,15 +3,17 @@ int x = 69420;
 int main() {
 	printf("Hello World!");
 	int i = 69420;
-	return 0;
 	 x = 420;
 	 x = 69;
-	 i = 420;
+	 i = 420; 
 	char testing[] = "This is a test of my stupidity";
 	char rein[] = " Reinforcement learning is fun, right?";
+	for (i+1; i>0;i--) {
+		printf("Your int is: %d\n", i);
+	}
 
 
 
-
+	return 0;
 
 }
